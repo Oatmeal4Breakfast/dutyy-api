415a549cb3d88671ac9e161ed9f6b828704cf7f5
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-06-06 16:01:25 -0400

    feature: new Domain Exceptions

--- src/domain/exceptions.py
+++ src/domain/exceptions.py
@@ -1,4 +1,19 @@
+from uuid import UUID
+
+
 class DomainValidationError(Exception):
     def __init__(self, entity: str, errors: list[str]) -> None:
         self.errors = errors
         super().__init__(f"{entity} validation failed {','.join(self.errors)}")
+
+
+class DutyyAssignedError(Exception):
+    def __init__(self, id: UUID) -> None:
+        self.id = id
+        super().__init__(f"Dutyy with id {self.id} already assigned to project")
+
+
+class DutyyNotAssignedError(Exception):
+    def __init__(self, id: UUID) -> None:
+        self.id = id
+        super().__init__(f"Dutyy with id {self.id} is not assigned to project")
