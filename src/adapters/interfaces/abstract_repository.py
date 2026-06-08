7d19429aca3f693317395ac2b7dfe5586752686b
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-06-07 23:12:14 -0400

    feature: add interface for Repos

---
+++ src/adapters/interfaces/abstract_repository.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractRepository[T](ABC):
+    @abstractmethod
+    async def get(self) -> list[T]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def delete(self) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def add(self) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def update(self) -> None:
+        raise NotImplementedError
