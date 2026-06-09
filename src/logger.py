d34c3ba4f7172dd1f71560289a5ad6f4d7e01924
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-06-08 22:32:01 -0400

    feature: add logger

--- src/logger.py
+++ src/logger.py
@@ -7,6 +7,8 @@ from src.config import ENV
 
 if TYPE_CHECKING:
     from src.config import Config
+    from structlog.processors import JSONRenderer
+    from structlog.dev import ConsoleRenderer
 
 
 def config_logger(config: Config) -> None:
@@ -16,7 +18,29 @@ def config_logger(config: Config) -> None:
     else:
         log_level = logging.INFO
 
+    shared_processers: list = [
+        structlog.contextvars.merge_contextvars,
+        structlog.stdlib.add_log_level,
+        structlog.stdlib.add_logger_name,
+        structlog.processors.TimeStamper("iso"),
+    ]
+
+    renderer: ConsoleRenderer | JSONRenderer = (
+        structlog.dev.ConsoleRenderer()
+        if config.env == ENV.DEVELOPMENT
+        else structlog.processors.JSONRenderer()
+    )
+
+    formatter = structlog.stdlib.ProcessorFormatter(
+        foreign_pre_chain=shared_processers,
+        processors=[
+            structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+            renderer,
+        ],
+    )
+
     stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
 
     root_logger = logging.getLogger()
     root_logger.setLevel(log_level)
@@ -24,8 +48,15 @@ def config_logger(config: Config) -> None:
 
     structlog.configure(
         processors=[
-            structlog.contextvars.merge_contextvars,
-            structlog.stdlib.add_log_level,
-            structlog.stdlib.add_logger_name,
-        ]
+            *shared_processers,
+            structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+        ],
+        wrapper_class=structlog.make_filtering_bound_logger(min_level=log_level),
+        context_class=dict,
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        cache_logger_on_first_use=config.env == ENV.PRODUCTION,
     )
+
+
+def get_logger(name: str) -> structlog.stdlib.BoundLogger:
+    return structlog.getLogger(name)
