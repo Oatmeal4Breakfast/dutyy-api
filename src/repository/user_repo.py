ea8ee770facb587ac60b110631b35408dbc04acf
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-06-14 19:49:31 -0400

    refactor: make it look more like the abc

--- src/repository/user_repo.py
+++ src/repository/user_repo.py
@@ -1,20 +1,21 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, Sequence
 from enum import StrEnum, auto
 
 from sqlalchemy.exc import IntegrityError, OperationalError
 from sqlalchemy import update, delete, select
+
+from src.repository.abstract_repository import AbstractRepository
+from src.db.orm import users_table
+from src.domain.user import User
+from src.logger import get_logger
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
     from uuid import UUID
     from sqlalchemy import Select, Result, RowMapping
 
-from src.repository.abstract_repository import AbstractRepository
-from src.db.orm import users_table
-from src.domain.user import User
-from src.logger import get_logger
 
 logger = get_logger(__name__)
 
@@ -29,6 +30,38 @@ class UserRepoErrorEvents(StrEnum):
 class UserRepo(AbstractRepository[User]):
     def __init__(self, session: AsyncSession) -> None:
         self._session = session
+
+    async def get_all(self, page: int = 1, page_size: int = 100) -> list[User]:
+        offset_value: int = (page - 1) * page_size
+
+        stmt: Select[Any] = (
+            select(users_table)
+            .order_by(users_table.c.id)
+            .limit(page_size)
+            .offset(offset_value)
+        )
+
+        try:
+            results: Result[Any] = await self._session.execute(stmt)
+        except OperationalError:
+            logger.error(event=UserRepoErrorEvents.DB_UNAVAILABLE, op="user_get")
+            raise
+
+        rows: Sequence[RowMapping] = results.mappings().all()
+        return [User(**row) for row in rows]
+
+    async def delete(self, entity: User) -> None:
+        stmt = delete(users_table).where(users_table.c.id == entity.id)
+
+        try:
+            await self._session.execute(stmt)
+            await self._session.flush()
+        except IntegrityError:
+            logger.error(event=UserRepoErrorEvents.USER_DELETE_ERROR, user_id=entity.id)
+            raise
+        except OperationalError:
+            logger.error(event=UserRepoErrorEvents.DB_UNAVAILABLE, op="user_delete")
+            raise
 
     async def add(self, entity: User) -> None:
         self._session.add(entity)
@@ -46,24 +79,18 @@ class UserRepo(AbstractRepository[User]):
             logger.error(event=UserRepoErrorEvents.DB_UNAVAILABLE, op="user_add")
             raise
 
-    async def get_all(self, page: int = 1, page_size: int = 100) -> list[User]:
-        offset_value: int = (page - 1) * page_size
-
-        stmt = (
-            select(users_table)
-            .order_by(users_table.c.id)
-            .limit(page_size)
-            .offset(offset_value)
-        )
-
+    async def update(self, entity: User) -> None:
+        data: dict[str, Any] = entity.to_dict()
+        stmt = update(users_table).where(users_table.c.id == entity.id).values(**data)
         try:
-            results = await self._session.execute(stmt)
-        except OperationalError:
-            logger.error(event=UserRepoErrorEvents.DB_UNAVAILABLE, op="user_get")
+            await self._session.execute(stmt)
+            await self._session.flush()
+        except IntegrityError:
+            logger.error(event=UserRepoErrorEvents.USER_UPDATE_ERROR, user_id=entity.id)
             raise
-
-        rows = results.mappings().all()
-        return [User(**row) for row in rows]
+        except OperationalError:
+            logger.error(event=UserRepoErrorEvents.DB_UNAVAILABLE, op="user_update")
+            raise
 
     async def get_by_id(self, user_id: UUID) -> User | None:
         stmt: Select[tuple[Any]] = select(users_table).where(
@@ -79,29 +106,3 @@ class UserRepo(AbstractRepository[User]):
         row: RowMapping | None = result.mappings().one_or_none()
 
         return User(**row) if row is not None else None
-
-    async def update(self, entity: User) -> None:
-        data: dict[str, Any] = entity.to_dict()
-        stmt = update(users_table).where(users_table.c.id == entity.id).values(**data)
-        try:
-            await self._session.execute(stmt)
-            await self._session.flush()
-        except IntegrityError:
-            logger.error(event=UserRepoErrorEvents.USER_UPDATE_ERROR, user_id=entity.id)
-            raise
-        except OperationalError:
-            logger.error(event=UserRepoErrorEvents.DB_UNAVAILABLE, op="user_update")
-            raise
-
-    async def delete(self, entity: User) -> None:
-        stmt = delete(users_table).where(users_table.c.id == entity.id)
-
-        try:
-            await self._session.execute(stmt)
-            await self._session.flush()
-        except IntegrityError:
-            logger.error(event=UserRepoErrorEvents.USER_DELETE_ERROR, user_id=entity.id)
-            raise
-        except OperationalError:
-            logger.error(event=UserRepoErrorEvents.DB_UNAVAILABLE, op="user_delete")
-            raise
