0efefeb55bce2e2fe4834467fa749e5bb0a6e699
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-06-20 16:42:32 -0400

    feat: complete the test_file

--- tests/repository/test_project_repo.py
+++ tests/repository/test_project_repo.py
@@ -18,6 +18,7 @@ class TestProjectRepo:
         results: list[Project] = await repo.get_all()
 
         assert len(results) == 1
+        assert isinstance(results[0], Project)
         assert results[0].id == project.id
 
     async def test_get_all_projects_returns_empty_list(self, session):
@@ -31,6 +32,7 @@ class TestProjectRepo:
 
         existing: list[Project] = await repo.get_all()
         assert len(existing) == 1
+        assert isinstance(existing[0], Project)
 
         await repo.delete(project)
 
@@ -52,6 +54,7 @@ class TestProjectRepo:
         results: list[Project] = await repo.get_all()
 
         assert len(results) == 1
+        assert isinstance(results[0], Project)
         assert results[0].id == project.id
         assert results[0].owner_id == user.id
 
@@ -67,6 +70,7 @@ class TestProjectRepo:
         result: Project | None = await repo.get_by_id(project.id)
 
         assert result is not None
+        assert isinstance(result, Project)
         assert project.status == ProjectStatus.IN_PROGRESS
 
     async def test_get_by_id_success(self, session, project):
@@ -86,7 +90,44 @@ class TestProjectRepo:
 
         assert result is None
 
-    # async def test_search_by_name_returns_non_empty_list(self, session, project, user):
-    #     repo = ProjectRepo(session)
-    #
-    #     second_project = make_project(name="Second Test ", owner_id=user.id)
+    async def test_search_by_name_returns_non_empty_list(self, session, project, user):
+        repo = ProjectRepo(session)
+
+        second_project = make_project(name="Second Test ", owner_id=user.id)
+
+        await repo.add(second_project)
+
+        results: list[Project] = await repo.search_by_name("Test", owner_id=user.id)
+
+        assert isinstance(results, list)
+        assert isinstance(results[0], Project)
+        assert len(results) == 2
+
+    async def test_search_by_name_returns_empty_list(self, session, user):
+        repo = ProjectRepo(session)
+
+        results: list[Project] = await repo.search_by_name("Test", owner_id=user.id)
+
+        assert len(results) == 0
+
+    async def test_get_by_owner_id_returns_non_empty_list(self, session, project, user):
+        repo = ProjectRepo(session)
+
+        results: list[Project] = await repo.get_by_owner_id(user.id)
+
+        assert len(results) == 1
+        assert isinstance(results[0], Project)
+
+    async def test_get_by_owner_id_returns_empty_list(self, session, user):
+        repo = ProjectRepo(session)
+
+        results: list[Project] = await repo.get_by_owner_id(user.id)
+
+        assert len(results) == 0
+
+    async def test_get_projects_by_user_id_success(self, session, project, user):
+        repo = ProjectRepo(session)
+        results: list[Project] = await repo.get_projects_by_user_id(user.id)
+
+        assert len(results) == 1
+        assert isinstance(results[0], Project)
