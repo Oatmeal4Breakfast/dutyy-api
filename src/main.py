5079818704ad4d89dbda1e7f6a1dadbbb94f365b
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-06-21 00:02:16 -0400

    chore: boot strap engine to the application

--- src/main.py
+++ src/main.py
@@ -1,6 +1,21 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
 from fastapi import FastAPI
+from src.db.db import create_engine_and_session
+from src.config import get_config
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
 
 app = FastAPI(title="dutyy-api")
+
+
+async def lifespan(app: FastAPI):
+    engine, session = create_engine_and_session(get_config())
+    app.state.session_factory: async_sessionmaker[AsyncSession] = session
+    yield
+    engine.dispose()
 
 
 @app.get("/")
