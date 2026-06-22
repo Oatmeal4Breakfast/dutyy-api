64125080c2e99dc7563a2d513c93f2d7d848cf27
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-06-21 22:42:52 -0400

    feat: add new User Domain events

--- src/domain/events.py
+++ src/domain/events.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 if TYPE_CHECKING:
     from uuid import UUID
     from datetime import datetime
+    from src.domain.user import UserStatus
 
 
 class Event:
@@ -43,3 +44,28 @@ class DutyyRemoved(Event):
 class OwnershipTransferred:
     project_id: UUID
     new_owner: UUID
+
+
+@dataclass(frozen=True)
+class UserCreated:
+    user_id: UUID
+    email: str
+    full_name: str
+    created_date: datetime
+
+
+@dataclass(frozen=True)
+class UserPasswordReset:
+    user_id: UUID
+    email: str
+    full_name: str
+    reqested_date: datetime
+
+
+@dataclass(frozen=True)
+class UserStatusChanged:
+    user_id: UUID
+    email: str
+    full_name: str
+    new_status: UserStatus
+    modified_date: datetime
