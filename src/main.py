cb047146a80ef45d5c87e1822236f9eda6b82b34
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-06-22 22:51:55 -0400

    feat: add application level exception handling

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request
 from fastapi.responses import JSONResponse
+from sqlalchemy.exc import OperationalError
 
 from src.api.deps import get_uow
+from src.domain.exceptions import (
+    DomainValidationError,
+    UserAlreadyExistsError,
+    ProjectAlreadyExistsError,
+)
 from src.db.db import create_engine_and_session
 from src.config import get_config
 
@@ -20,6 +26,34 @@ async def lifespan(app: FastAPI):
     app.state.session_factory: async_sessionmaker[AsyncSession] = session
     yield
     engine.dispose()
+
+
+@app.exception_handler(DomainValidationError)
+async def domain_validation_exception_handler(
+    request: Request, exc: DomainValidationError
+) -> JSONResponse:
+    return JSONResponse(status_code=422, content={"errors": exc.errors})
+
+
+@app.exception_handler(UserAlreadyExistsError)
+async def user_already_exist_handler(
+    request: Request, exc: UserAlreadyExistsError
+) -> JSONResponse:
+    return JSONResponse(status_code=409, content={"detail": str(exc)})
+
+
+@app.exception_handler(ProjectAlreadyExistsError)
+async def project_already_exist_handler(
+    request: Request, exc: ProjectAlreadyExistsError
+) -> JSONResponse:
+    return JSONResponse(status_code=409, content={"detail": str(exc)})
+
+
+@app.exception_handler(OperationalError)
+async def operation_error_handler(
+    request: Request, exc: OperationalError
+) -> JSONResponse:
+    return JSONResponse(status_code=503, content={"detail": "service unavailable"})
 
 
 @app.get("/")
