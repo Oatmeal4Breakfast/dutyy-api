9a2523b1f9b620ab3d37dadec79efa76af1736bc
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-06-26 23:46:31 -0400

    feat: add test for UserService.update_user() method

--- tests/services/test_user_service.py
+++ tests/services/test_user_service.py
@@ -2,17 +2,34 @@ import pytest
 
 from unittest.mock import AsyncMock
 
-from src.domain.events import UserCreated
+from src.domain.events import UserCreated, UserStatusChanged
+from src.domain.user import UserStatus, UserUpdateFields
 from tests.conftest import make_user_service, FakeUnitOfWork
 
 
 @pytest.mark.integration
-async def test_create_user_fires_event(session, event_bus):
-    fake_handler = AsyncMock()
-    event_bus.subscribe(UserCreated, fake_handler)
-    uow = FakeUnitOfWork(session, event_bus)
-    service = make_user_service(uow)
+class TestUserService:
+    async def test_create_user_fires_event(self, session, event_bus):
+        fake_handler = AsyncMock()
+        event_bus.subscribe(UserCreated, fake_handler)
+        uow = FakeUnitOfWork(session, event_bus)
+        service = make_user_service(uow)
 
-    await service.create_user(fname="test", lname="test", email="test@example.com")
+        await service.create_user(fname="test", lname="test", email="test@example.com")
 
-    fake_handler.assert_called_once()
+        fake_handler.assert_called_once()
+
+    async def test_update_user_fires_events(self, user, session, event_bus):
+        fake_user_status_handler = AsyncMock()
+
+        event_bus.subscribe(UserStatusChanged, fake_user_status_handler)
+
+        uow = FakeUnitOfWork(session, event_bus)
+
+        service = make_user_service(uow)
+
+        changes = UserUpdateFields(status=UserStatus.INACTIVE)
+
+        await service.update_user(user_id=user.id, changes=changes)
+
+        fake_user_status_handler.assert_called_once()
