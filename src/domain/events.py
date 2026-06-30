18803ffa7e128201e2d21811fe11239741d1f1ab
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-06-30 10:15:38 -0400

    fix: events weren't being created correctly

--- src/domain/events.py
+++ src/domain/events.py
@@ -5,7 +5,6 @@ from uuid import UUID, uuid4
 from datetime import datetime, UTC
 
 if TYPE_CHECKING:
-    from datetime import datetime
     from src.domain.user import UserStatus
 
 
@@ -20,57 +19,65 @@ class Event:
 @dataclass(frozen=True)
 class DutyyCompleted(Event):
     duty_id: UUID
-    duty_name: str
-    project_name: str
-    completed_by: str
+    created_date: datetime
+    completed_date: datetime
 
 
 @dataclass(frozen=True)
 class ProjectCompleted(Event):
     project_id: UUID
     created_date: datetime
+    completed_date: datetime
 
 
 @dataclass(frozen=True)
 class DutyyAdded(Event):
     dutyy_id: UUID
     project_id: UUID
+    modified_date: datetime
 
 
 @dataclass(frozen=True)
 class DutyyRemoved(Event):
     dutyy_id: UUID
     project_id: UUID
+    modified_date: datetime
 
 
 @dataclass(frozen=True)
-class OwnershipTransferred:
+class OwnershipTransferred(Event):
     project_id: UUID
     new_owner: UUID
 
 
 @dataclass(frozen=True)
-class UserCreated:
-    user_id: UUID
-
-
-@dataclass(frozen=True)
-class UserPasswordReset:
+class UserCreated(Event):
     user_id: UUID
     email: str
     full_name: str
+    created_date: datetime
 
 
 @dataclass(frozen=True)
-class UserStatusChanged:
+class UserPasswordReset(Event):
+    user_id: UUID
+    email: str
+    full_name: str
+    reqested_date: datetime
+
+
+@dataclass(frozen=True)
+class UserStatusChanged(Event):
     user_id: UUID
     email: str
     full_name: str
     new_status: UserStatus
+    modified_date: datetime
 
 
 @dataclass(frozen=True)
 class PasswordHashCreated(Event):
     user_id: UUID
     user_email: str
+    first_name: str
     plain_text_password: str
