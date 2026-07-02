22ced2bc5837a2fc8e0a22944948098c53d79574
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-07-02 12:41:17 -0400

    refactor: imports and add singleton to app startup for auth_service

--- src/main.py
+++ src/main.py
@@ -12,28 +12,43 @@ from src.domain.exceptions import (
     UserAlreadyExistsError,
     ProjectAlreadyExistsError,
 )
+from src.domain.events import UserCreated, PasswordHashCreated
 from src.bus.bus import EventBus
 from src.service.user_service import UserNotFoundError
 from src.service.auth_service import AuthService
+from src.service.email_service import EmailService
 from src.db.db import create_engine_and_session
-from src.config import get_config, get_auth_service_config, AuthServiceConfig, Config
+from src.config import (
+    get_config,
+    get_auth_service_config,
+    get_email_service_config,
+)
 from src.logger import config_logger
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
     from src.db.uow import UnitOfWork
+    from src.config import AuthServiceConfig, Config, EmailServiceConfig
 
 
 async def lifespan(app: FastAPI):
     config: Config = get_config()
-    auth_config: AuthServiceConfig = get_auth_service_config()
     config_logger(config)
+    auth_config: AuthServiceConfig = get_auth_service_config()
+    email_config: EmailServiceConfig = get_email_service_config()
     engine, session = create_engine_and_session(config)
-    app.state.session_factory: async_sessionmaker[AsyncSession] = session
-    app.state.event_bus: EventBus = EventBus()
-    app.state.auth_service = AuthService(
-        app.state.session_factory, app.state.event_bus, auth_config
+    bus = EventBus()
+    auth_service = AuthService(
+        session_factory=session, event_bus=bus, auth_service_config=auth_config
     )
+
+    email_service = EmailService(email_config)
+    app.state.session_factory: async_sessionmaker[AsyncSession] = session
+    app.state.event_bus: EventBus = bus
+    app.state.auth_service = auth_service
+    app.state.email_service = email_service
+    bus.subscribe(UserCreated, auth_service.handle_user_created)
+    bus.subscribe(PasswordHashCreated, email_service.send_welcome_email)
     yield
     await engine.dispose()
 
