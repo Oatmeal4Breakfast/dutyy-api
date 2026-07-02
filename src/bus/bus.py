3ca9e3d6daa205348f7e3f4181c672afb1e616a0
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-07-02 12:40:51 -0400

    refactor: update the typing the to be a bit more generic

--- src/bus/bus.py
+++ src/bus/bus.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import asyncio
-from typing import Protocol, Callable, Coroutine, TYPE_CHECKING, Any
+from typing import Protocol, Callable, TYPE_CHECKING, Awaitable
 
 from src.logger import get_logger
 
@@ -10,32 +10,38 @@ if TYPE_CHECKING:
 
 logger = get_logger(__name__)
 
-type EventHandler = Callable[[Event], Coroutine[Any, Any, None]]
-
 
 class EventBusProtocol(Protocol):
-    def subscribe(self, event_type: type[Event], handler: EventHandler) -> None: ...
-    def unsubscribe(self, event: type[Event], handler: EventHandler) -> None: ...
+    def subscribe[E: Event](
+        self, event_type: type[E], handler: Callable[[E], Awaitable]
+    ) -> None: ...
+    def unsubscribe[E: Event](
+        self, event: type[E], handler: Callable[[E], Awaitable]
+    ) -> None: ...
     async def publish(self, event: Event) -> None: ...
 
 
 class EventBus:
     def __init__(self) -> None:
-        self._handlers: dict[type[Event], list[EventHandler]] = {}
+        self._handlers: dict[type[Event], list[Callable]] = {}
 
-    def subscribe(self, event_type: type[Event], handler: EventHandler):
+    def subscribe[E: Event](
+        self, event_type: type[E], handler: Callable[[E], Awaitable]
+    ):
         if event_type not in self._handlers:
             self._handlers[event_type] = []
         self._handlers[event_type].append(handler)
         logger.info(event="new_event_handler_registered", type=event_type)
 
-    def unsubscribe(self, event_type: type[Event], handler: EventHandler) -> None:
+    def unsubscribe[E: Event](
+        self, event_type: type[E], handler: Callable[[E], Awaitable]
+    ) -> None:
         if event_type in self._handlers and handler in self._handlers[event_type]:
             self._handlers[event_type].remove(handler)
         logger.info(event="handler_unsubscribed_from_event", event_type=event_type)
 
     async def publish(self, event: Event) -> None:
-        handlers: list[EventHandler] = self._handlers.get(type(event), []).copy()
+        handlers: list[Callable] = self._handlers.get(type(event), []).copy()
 
         if not handlers:
             logger.warn(event="no_handlers_for_event", event_type=type(event).__name__)
@@ -47,8 +53,6 @@ class EventBus:
             handler_count=len(handlers),
         )
 
-        tasks: list[Coroutine[Any, Any, None]] = [
-            handler(event) for handler in handlers
-        ]
+        tasks: list[Awaitable] = [handler(event) for handler in handlers]
 
         await asyncio.gather(*tasks)
