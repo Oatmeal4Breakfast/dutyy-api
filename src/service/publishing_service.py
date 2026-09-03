77fbc7991e73b1ff027cfec27d3c91022f5555ee
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-09-02 21:02:06 -0400

    feat: start publishing service

--- src/service/publishing_service.py
+++ src/service/publishing_service.py
@@ -1,12 +1,19 @@
 from __future__ import annotations
-from typing import Callable, TYPE_CHECKING
+
+from collections.abc import Callable
+from typing import TYPE_CHECKING
 
 from src.db.uow import AbstractUnitOfWork
+from src.domain.dutyy import Dutyy
+from src.domain.project import Project
 from src.logger import get_logger
 
 if TYPE_CHECKING:
+    from uuid import UUID
+
+    from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+
     from src.bus.bus import EventBus
-    from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
 
 logger = get_logger(__name__)
 
@@ -18,4 +25,39 @@ class PublishingService:
             [async_sessionmaker[AsyncSession], EventBus], AbstractUnitOfWork
         ],
     ) -> None:
-        pass
+        self._uow_factory: Callable = uow_factory
+
+    async def create_project_draft(
+        self, project_name: str, owner_id: UUID, dutyys: list[Dutyy] | None = None
+    ) -> Project:
+        project = Project(
+            name=project_name,
+            owner_id=owner_id,
+            dutyys=dutyys if dutyys is not None else [],
+        )
+        async with self._uow_factory() as uow:
+            await uow.project.add(project)
+            await uow.commit()
+            logger.info(
+                event="new_project_created",
+                project_id=str(project.id),
+                owner_id=str(owner_id),
+            )
+        return project
+
+    async def publish_project(self, project_id: UUID) -> None:
+        async with self._uow_factory() as uow:
+            project: Project | None = await uow.project.get_by_id(project_id=project_id)
+
+            if project is None:
+                logger.warning(event="project_not_found", project_id=str(project_id))
+                return
+
+            if len(project.dutyys) <= 0:
+                logger.warning(
+                    event="project_has no dutyys",
+                    project_id=str(project.id),
+                )
+                return
+
+            project.publish()
